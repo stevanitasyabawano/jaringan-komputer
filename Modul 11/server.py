83b642f3a19783ba5e302f9756d8405c8b893ef5
stevanitasyabawano <91860500+stevanitasyabawano@users.noreply.github.com>
2022-11-28 09:24:39 +0700

    Create server.py

---
+++ Modul 11/server.py
@@ -0,0 +1,7 @@
+(base) PS C:\Users\hp> cd ..
+(base) PS C:\Users> cd ..
+(base) PS C:\> cd Python2
+(base) PS C:\Python2> server.py
+(base) PS C:\Python2>
+(base) PS C:\Python2> server.py
+(base) PS C:\Python2>
